f72c0dfc44f698224aaad25901944db9b0915760
tonofprince <brettleibo@gmail.com>
2014-11-07 19:18:58 -0800

    collision code done

--- watchout.js
+++ watchout.js
@@ -18,29 +18,31 @@ var axes = {
   y: d3.scale.linear().domain([0,100]).range([0,gameOptions.height])
 };
 
+var updateScore = function() {
+  d3.select('.current')
+    .select('span')
+    .text(gameStats.currentScore.toString());
+};
+
+var updateHighScore = function() {
+  if (gameStats.currentScore > gameStats.highScore) {
+    gameStats.highScore = gameStats.currentScore;
+    d3.select('.high')
+    .select('span')
+    .text(gameStats.currentScore.toString());
+  }
+};
+
+var updateCollisions = function() {
+  gameStats.collisions++;
+  d3.select('.collisions')
+    .select('span')
+    .text(gameStats.collisions.toString());
+};
 
 var gameBoard = d3.select('.container').append('svg:svg')
                   .attr('width', gameOptions.width)
                   .attr('height', gameOptions.height);
-
-// var playerDat = {
-//   x: 50,
-//   y: 50,
-// };
-
-// var playerData = [playerDat];
-
-// var player = gameBoard.selectAll('circle.player')
-//                       .data(playerData, function(d,i) {return i;});
-
-// player.enter()
-//       .append('svg:circle')
-//       .attr('class', 'player')
-//       .attr('cx', function(player) {return axes.x(player.x);})
-//       .attr('cy', function(player) {return axes.y(player.y);})
-//       .attr('r', 10)
-//       .attr('fill', 'gold')
-//       .call(drag);
 
 var dragmove = function(d) {
   d3.select(this)
@@ -66,7 +68,7 @@ var createEnemies = function() {
   var enemies = [];
   for (var i = 0; i < gameOptions.nEnemies; i++) {
     enemies.push({
-      //id : i,
+      id : i,
       x : Math.random() * 100,
       y : Math.random() * 100
     });
@@ -74,30 +76,47 @@ var createEnemies = function() {
   return enemies;
 };
 
-// var setupDragging = function() {
-//   var that = this;
-//   var drag = d3.behavior.drag().on('drag', function() {
-//     that.moveRelative(d3.event.dx, d3.event.dy);
-//   });
-//   return this.el.call(drag);
-// };
-
-
-
+var touchMap = {};
 var render = function(enemy_data) {
   var enemies = gameBoard.selectAll('circle.enemy')
                          .data(enemy_data, function(d,i) {return i;} );
-  // console.log(enemy_data);
 
   enemies.enter()
          .append('svg:circle')
          .attr('class', 'enemy')
          .attr('cx', function(enemy) {return axes.x(enemy.x);})
          .attr('cy', function(enemy) {return axes.y(enemy.y);})
+         .attr('id', function(enemy) {return enemy.id;})
          .attr('r',0);
 
   enemies.exit()
          .remove();
+
+
+
+  var checkCollision = function(enemy, callback) {
+
+    var player = gameBoard.selectAll('circle.player');
+    var radiusSum = parseFloat(enemy.attr('r')) + parseFloat(player.attr('r'));
+    var xDiff = parseFloat(enemy.attr('cx')) - parseFloat(player.attr('cx'));
+    var yDiff = parseFloat(enemy.attr('cy')) - parseFloat(player.attr('cy'));
+    var separation = Math.sqrt( Math.pow(xDiff,2) + Math.pow(yDiff,2));
+    if (separation <= radiusSum && !touchMap[enemy.attr('id')]) {
+      callback(enemy);
+      touchMap[enemy.attr('id')] = true;
+    }
+    else if (touchMap[enemy.attr('id')] === true && separation > radiusSum) {
+      touchMap[enemy.attr('id')] = false;
+    }
+  };
+
+  var onCollision = function(enemy){
+    updateHighScore();
+    enemy.attr('fill', 'blue');
+    gameStats.currentScore = 0;
+    updateScore();
+    updateCollisions();
+  };
 
   var tweenWithCollisionDetection = function(endData) {
     var enemy = d3.select(this);
@@ -113,6 +132,9 @@ var render = function(enemy_data) {
     };
 
     return function(t) {
+
+      checkCollision(enemy, onCollision);
+
       var enemyNextPos = {
         x : startPos.x + (endPos.x - startPos.x)*t,
         y : startPos.y + (endPos.y - startPos.y)*t
@@ -136,37 +158,18 @@ var render = function(enemy_data) {
 
 var play = function() {
   var gameTurn = function() {
-    //debugger;
     var newEnemyPositions = createEnemies();
     render(newEnemyPositions);
   };
 
   //gameTurn();
-  //debugger;
   setInterval(gameTurn, 2000);
+
+  setInterval(function() {
+    gameStats.currentScore++;
+    updateScore();
+  }, 50);
 
 };
 
 play();
-
-
-
-// var circle = d3.select('svg')
-//                .append('svg:circle')
-//                .attr('cx', 50)
-//                .attr('cy', 50)
-//                .attr('r', 10);
-
-// var dot = d3.select('svg')
-//                .append('svg:circle')
-//                .attr('cx', 50)
-//                .attr('cy', 50)
-//                .attr('r', 5)
-//                .style('fill', 'green');
-
-// var player = d3.select('svg')
-//                .append('svg:circle')
-//                .attr('cx', 80)
-//                .attr('cy', 80)
-//                .attr('r', 5)
-//                .style('fill', 'gold');
