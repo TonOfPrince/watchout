ad04416764f1e424e92ba1a4482704aea1deb3a3
tonofprince <brettleibo@gmail.com>
2014-11-07 17:03:51 -0800

    draggable player

--- watchout.js
+++ watchout.js
@@ -23,6 +23,45 @@ var gameBoard = d3.select('.container').append('svg:svg')
                   .attr('width', gameOptions.width)
                   .attr('height', gameOptions.height);
 
+// var playerDat = {
+//   x: 50,
+//   y: 50,
+// };
+
+// var playerData = [playerDat];
+
+// var player = gameBoard.selectAll('circle.player')
+//                       .data(playerData, function(d,i) {return i;});
+
+// player.enter()
+//       .append('svg:circle')
+//       .attr('class', 'player')
+//       .attr('cx', function(player) {return axes.x(player.x);})
+//       .attr('cy', function(player) {return axes.y(player.y);})
+//       .attr('r', 10)
+//       .attr('fill', 'gold')
+//       .call(drag);
+
+var dragmove = function(d) {
+  d3.select(this)
+    .attr('cx', d3.event.x)
+    .attr('cy', d3.event.y);
+};
+
+var drag = d3.behavior.drag()
+    .on('drag', dragmove);
+
+d3.select('svg')
+  .append('svg:circle')
+  .attr('class', 'player')
+  .attr('cx', 50)
+  .attr('cy', 50)
+  .attr('r', 10)
+  .attr('fill', 'gold')
+  .call(drag);
+
+
+
 var createEnemies = function() {
   var enemies = [];
   for (var i = 0; i < gameOptions.nEnemies; i++) {
@@ -35,11 +74,20 @@ var createEnemies = function() {
   return enemies;
 };
 
+// var setupDragging = function() {
+//   var that = this;
+//   var drag = d3.behavior.drag().on('drag', function() {
+//     that.moveRelative(d3.event.dx, d3.event.dy);
+//   });
+//   return this.el.call(drag);
+// };
+
+
 
 var render = function(enemy_data) {
   var enemies = gameBoard.selectAll('circle.enemy')
                          .data(enemy_data, function(d,i) {return i;} );
-  console.log(enemy_data);
+  // console.log(enemy_data);
 
   enemies.enter()
          .append('svg:circle')
@@ -80,7 +128,7 @@ var render = function(enemy_data) {
       .duration(500)
       .attr('r',10)
     .transition()
-      .duration(1000)
+      .duration(2000)
       .tween('custom', tweenWithCollisionDetection);
 
 
@@ -94,8 +142,8 @@ var play = function() {
   };
 
   //gameTurn();
-  debugger;
-  setInterval(gameTurn, 1000);
+  //debugger;
+  setInterval(gameTurn, 2000);
 
 };
 
